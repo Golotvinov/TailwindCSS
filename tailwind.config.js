c4144cac72a510e74af82066c9a95514438bcdb1
Aleks Golotvinov <golotvinov@gmail.com>
2021-07-03 17:35:29 +0200

    Sekce O nás + rozšířená barevná paleta

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      teal: colors.teal,
+      cyan: colors.cyan,
+    },
     extend: {},
   },
   variants: {
